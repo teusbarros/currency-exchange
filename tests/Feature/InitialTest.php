e39c6619ede29bfdd92ffc49a793cc95a0bf5cac
Mateus Pinheiro <teusbarros@Mateuss-MacBook-Pro.local>
2023-04-03 22:14:51 -0300

    Test phpunit

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace teusbarros\CurrencyExchange\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    public function test_my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
